524df627cc2ac9b9973fa2a4bf5c8de62f06b3b8
1055373165 <1055373165@qq.com>
2022-07-23 15:28:18 +0800

    v2.0.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/1055373165/testMod/v2
+
+go 1.18
---
+++ testmod.go
@@ -0,0 +1,7 @@
+package testMod
+
+import "fmt"
+
+func Hello(name string, str string) {
+	fmt.Printf("hello, %s, %s", name, str)
+}
